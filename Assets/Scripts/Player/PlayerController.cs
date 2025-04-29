d3c5953f0acd47e3d7bf71fe3f3bd591d0243bbe
Hisham Ata <hisham@catsplatstudios.com>
2025-04-29 10:15:02 -0400

    Update PlayerController.cs

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -75,4 +75,33 @@ public class PlayerController : MonoBehaviour
         //if ((hInput > 0 && sr.flipX) || (hInput < 0 && !sr.flipX)) sr.flipX = !sr.flipX;
         
     }
+
+    public void OnCollisionEnter2D(Collision2D collision)
+    {
+        
+    }
+
+    public void OnCollisionExit2D(Collision2D collision)
+    {
+        
+    }
+
+    public void OnCollisionStay2D(Collision2D collision)
+    {
+        
+    }
+
+    public void OnTriggerEnter2D(Collider2D collision)
+    {
+
+    }
+    public void OnTriggerExit2D(Collider2D collision)
+    {
+        
+    }
+
+    public void OnTriggerStay2D(Collider2D collision)
+    {
+        
+    }
 }
