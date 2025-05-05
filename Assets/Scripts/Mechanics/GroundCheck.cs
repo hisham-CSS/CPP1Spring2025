ea7f4d3e16287cc04d7cb83ccda6ae4253d0e8e1
Hisham <hisham@catsplatstudios.com>
2025-05-05 09:47:35 -0400

    Update GroundCheck.cs

--- Assets/Scripts/Mechanics/GroundCheck.cs
+++ Assets/Scripts/Mechanics/GroundCheck.cs
@@ -2,15 +2,20 @@ using UnityEngine;
 
 public class GroundCheck
 {
+    //private property with public getter
     private bool isGrounded;
     public bool IsGrounded => isGrounded;
 
+    //private properties
     private LayerMask isGroundLayer;
     private Collider2D collider;
     private Rigidbody2D rb;
     private float groundCheckRadius;
 
+    //private property to get the ground check position
     private Vector2 groundCheckPos => new Vector2(collider.bounds.min.x + collider.bounds.extents.x, collider.bounds.min.y);
+
+    //Constructor to initialize the GroundCheck class
     public GroundCheck(LayerMask isGroundLayer, Collider2D collider, Rigidbody2D rb, ref float groundCheckRadius)
     {
         this.isGroundLayer = isGroundLayer;
@@ -19,12 +24,10 @@ public class GroundCheck
         this.groundCheckRadius = groundCheckRadius;
     }
 
+    //Method to check if the player is grounded
     public bool CheckIsGrounded()
     {
-        if (!isGrounded && rb.linearVelocityY <= 0)
-        {
-            isGrounded = Physics2D.OverlapCircle(groundCheckPos, groundCheckRadius, isGroundLayer);
-        }
+        if (!isGrounded && rb.linearVelocityY <= 0) isGrounded = Physics2D.OverlapCircle(groundCheckPos, groundCheckRadius, isGroundLayer);
         else if (isGrounded) isGrounded = Physics2D.OverlapCircle(groundCheckPos, groundCheckRadius, isGroundLayer);
 
         return isGrounded;
