3fcaf388c71f63fd9b308117bcceb96bb837523e
Hisham <hisham@catsplatstudios.com>
2025-05-06 14:00:29 -0400

    Lab 5 incomplete
    update pickups

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -1,3 +1,6 @@
+using System;
+using System.Collections;
+using Unity.Android.Gradle.Manifest;
 using UnityEngine;
 
 [RequireComponent(typeof(Rigidbody2D), typeof(SpriteRenderer), typeof(Animator))]
@@ -14,8 +17,67 @@ public class PlayerController : MonoBehaviour
     private Rigidbody2D rb;
     private SpriteRenderer sr;
     private Animator anim;
-    GroundCheck groundCheck;
-  
+    private GroundCheck groundCheck;
+
+    private Coroutine speedChange = null;
+
+    public void SpeedChange()
+    {
+        if (speedChange != null)
+        {
+            StopCoroutine(speedChange);
+            speedChange = null;
+            speed /= 2.0f;
+        }
+        
+        speedChange = StartCoroutine(SpeedChangeCoroutine());
+    }
+
+    IEnumerator SpeedChangeCoroutine()
+    {
+        speed *= 2.0f;
+        Debug.Log("Speed has been changed to: " + speed);
+
+        yield return new WaitForSeconds(5.0f);
+
+        speed /= 2.0f;
+        Debug.Log("Speed has been changed to: " + speed);
+    }
+
+    private int lives = 3;
+    public int Lives
+    {
+        get
+        {
+            return lives;
+        }
+        set
+        {
+            if (value < 0)
+            {
+                GameOver();
+                return;
+            }
+            if (lives > value)
+            {
+                Respawn();
+            }
+            lives = value;
+            Debug.Log("Lives have been set to: " + lives);
+        }
+    }
+
+    private void Respawn()
+    {
+        Debug.Log("Respawn goes here");
+    }
+
+    private void GameOver()
+    {
+        Debug.Log("Game Over goes here");
+    }
+
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -23,6 +85,10 @@ public class PlayerController : MonoBehaviour
         sr = GetComponent<SpriteRenderer>();
         anim = GetComponent<Animator>();
         groundCheck = new GroundCheck(LayerMask.GetMask("Ground"), GetComponent<Collider2D>(), rb, ref groundCheckRadius);
+
+        //SetLives(GetLives() + 1);
+
+        //Lives++;
     }
 
     // Update is called once per frame
@@ -66,14 +132,13 @@ public class PlayerController : MonoBehaviour
 
     void SpriteFlip(float hInput) 
     {
-        //if no input - we flip based on if input is less than zero - there is no real performance cost to setting sr.flipX every frame, however doing it in the follow two ways means that sr.flipX is set every frame there is an input
+        //if no input - we flip based on if input is less than zero - there is no real performance cost to setting sr.flipX every frame, however doing it in the following two ways means that sr.flipX is set every frame there is an input
         if (hInput != 0) sr.flipX = (hInput < 0);
         //if (hInput > 0) sr.flipX = false;
         //else if (hInput < 0) sr.flipX = true;
         
         //this is good as the sr.flipX is only changed when it needs too
         //if ((hInput > 0 && sr.flipX) || (hInput < 0 && !sr.flipX)) sr.flipX = !sr.flipX;
-        
     }
 
     public void OnCollisionEnter2D(Collision2D collision)
@@ -93,7 +158,8 @@ public class PlayerController : MonoBehaviour
 
     public void OnTriggerEnter2D(Collider2D collision)
     {
-
+        if (collision.CompareTag("Pickup"))
+            Destroy(collision.gameObject);
     }
     public void OnTriggerExit2D(Collider2D collision)
     {
